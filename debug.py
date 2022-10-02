e9690cd77029929a775adfef747e0e23251bed21
McFly <55802619+McflyBull@users.noreply.github.com>
2022-10-02 02:36:37 -0300

    heroku condig and btsoup test

--- debug.py
+++ debug.py
@@ -1,23 +1,42 @@
-from datetime import datetime
-from time import gmtime, strftime
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-print("Your Time Zone is GMT", strftime("%z", gmtime()))
+import requests
+from bs4 import BeautifulSoup
 
 
-import pytz  # 3rd party: $ pip install pytz
+def precioDolar():
+    #the fix
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    #response = requests.get(url, headers=headers)
+    
+    try:
+        print("Scrapping bna")
+        vgm_url = 'https://www.bna.com.ar/Cotizador/MonedasHistorico'
+        print("url for scrap created")
+        html_text = requests.get(vgm_url, headers=headers).text
+        print("creating html text")
+        soup = BeautifulSoup(html_text, 'html.parser')
+        print("converting with beatiful soup")
+        el = soup.select_one(
+            '#cotizacionesCercanas > table.table.table-bordered.cotizador > tbody > tr > td.dest')
+        # Dejo solo dos decimales, deberia hacerlo de otra manera
+        print("taking value with selector")
+        cotizacionDolar = float(el.get_text(strip=True))
+        print(cotizacionDolar)
+        return (cotizacionDolar)
+    except:
+        return ("An exception occurred")
 
 
 
 
+def get_new():
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    webToScrap = 'https://www.bna.com.ar/Cotizador/MonedasHistorico/'
+    asd = BeautifulSoup(requests.get(webToScrap, headers=headers).text, features="html.parser").select_one('#cotizacionesCercanas > table.table.table-bordered.cotizador > tbody > tr > td.dest')
+    cotizacionDolar = float(asd.get_text(strip=True))
+    
+    print(cotizacionDolar)
+    return cotizacionDolar
 
-
-
-
-dayNow = int(now.strftime("%d"))
-hourNow = int(now.strftime("%H"))
-
-print(dayNow)
-print(hourNow)
+print(precioDolar())
+print(get_new())
