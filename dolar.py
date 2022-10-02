eb4ef026b133e8fa79613a51f088d21b65a1d5b2
McFly <55802619+McflyBull@users.noreply.github.com>
2022-10-02 13:25:09 -0300

    .a

--- dolar.py
+++ dolar.py
@@ -3,39 +3,30 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import pytz #For zonetime
 
 def precioDolar():
-    #the fix
+    print("Scrapping bna")
+    scrapping_url = 'https://www.bna.com.ar/Cotizador/MonedasHistorico'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    #response = requests.get(url, headers=headers)
-    
     try:
-        print("Scrapping bna")
-        vgm_url = 'https://www.bna.com.ar/Cotizador/MonedasHistorico'
-        print("url for scrap created")
-        html_text = requests.get(vgm_url, headers=headers).text
-        print("creating html text")
+        print("converting the site to text")
+        html_text = requests.get(scrapping_url, headers=headers).text
+
+        print("Creating html parser")
         soup = BeautifulSoup(html_text, 'html.parser')
-        print("converting with beatiful soup")
+
+        print("Im looking for your selector")
         el = soup.select_one(
             '#cotizacionesCercanas > table.table.table-bordered.cotizador > tbody > tr > td.dest')
-        # Dejo solo dos decimales, deberia hacerlo de otra manera
+
         print("taking value with selector")
         cotizacionDolar = float(el.get_text(strip=True))
+        
         print(cotizacionDolar)
         return (cotizacionDolar)
     except:
         return ("An exception occurred")
-
-def timeArgNow():
-    print("Looking for datetime arg")
-    u = datetime.utcnow()
-    u = u.replace(tzinfo=pytz.utc) #NOTE: it works only with a fixed utc offset
-    now = u.astimezone(pytz.timezone("America/Argentina/Buenos_Aires"))
-    print(now)
-    return (now)
 
 def debug(cont):
     if cont > 99999:
@@ -45,7 +36,7 @@ def debug(cont):
     time.sleep(1)
     return (cont)
 
-#url_api = 'https://hostwebandapps.pythonanywhere.com/cotizacion/Dollar/'
+url_api = 'https://hostwebandapps.pythonanywhere.com/cotizacion/Dollar/'
 #url_api = 'http://127.0.0.1:8000/cotizacion/Dollar/'
 cotizationDidntCheked = True
 
@@ -56,7 +47,7 @@ while True:
     # Debug
     cont = debug(cont)
 
-    now = timeArgNow()
+    now = datetime.now()
     dayNow = int(now.strftime("%d"))
     hourNow = int(now.strftime("%H"))
 
@@ -68,8 +59,7 @@ while True:
         dollarPrice = precioDolar()
         if dollarPrice != 'An exception occurred':
             print(now)
-            r = requests.post(url_api, data={'price': dollarPrice,
-                                            'stamp': now})
+            r = requests.post(url_api, data={'price': dollarPrice})
             # check status code for response received. Success code - 200. Print content of request
             print(r, r.json())
             # Si ya consulto, evito que se repita
